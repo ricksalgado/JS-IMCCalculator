edff38a77efe1746fe27028dc964e6bfe81751bb
Ricardo Salgado <ricardodsalg@gmail.com>
2024-12-30 17:33:14 -0500

    JS first functions
    - getting the velues
    - adding clear button functionality
    - Adding valid Digits check

--- js/main.js
+++ js/main.js
@@ -37,3 +37,63 @@ const data = [
     },
 ];
 
+// element selction
+const bmiTable = document.querySelector('#bmi-table');
+
+const heightInput = document.querySelector('#height');
+const weightInput = document.querySelector('#weight');
+const calcBtn = document.querySelector('#calc-btn')
+const clearBtn = document.querySelector('#clear-btn')
+
+// Function
+function createTable(data) {
+    data.forEach((item) => {
+        const div = document.createElement('div');
+        div.classList.add('table-data');
+
+        const classification = document.createElement('p')
+        classification.innerText = item.classification;
+
+        const info = document.createElement('p')
+        info.innerText = item.info;
+
+        const obesity = document.createElement('p')
+        obesity.innerText = item.obesity;
+
+        div.appendChild(classification);
+        div.appendChild(info);
+        div.appendChild(obesity);
+
+        bmiTable.appendChild(div);
+    })
+};
+
+function clearInputs() {
+    heightInput.value = '';
+    weightInput.value = '';
+}
+
+function validDigits(text){
+    return text.replace(/[^0-9.]/g, "");
+}
+
+
+// Proj init
+createTable(data);
+
+// Events
+
+[heightInput, weightInput].forEach((el) => {
+    el.addEventListener("input", (e) => {
+        const updatedValue = validDigits(e.target.value);
+
+        e.target.value = updatedValue;
+    });
+});
+
+clearBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    clearInputs();
+});
+
