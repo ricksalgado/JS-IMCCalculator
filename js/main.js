8e5f1cae07fed9a5ea03d03ded7e46322d9f3264
Ricardo Salgado <ricardodsalg@gmail.com>
2024-12-30 16:29:40 -0500

    BMI data
    BMI Data Object input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-console.log(
-    'test'
-)
+// BMI DATA
+const data = [
+    {
+        min: 0,
+        max: 18.4,
+        classification: "Less than 18.5",
+        info: "Underweight",
+        obesity: "0",
+    },
+    {
+        min: 18.5,
+        max: 24.9,
+        classification: "Between 18.5 and 24.9",
+        info: "Normal",
+        obesity: "0",
+    },
+    {
+        min: 25,
+        max: 29.9,
+        classification: "Between 25.0 and 29.9",
+        info: "Overweight",
+        obesity: "I",
+    },
+    {
+        min: 30,
+        max: 39.9,
+        classification: "Between 30.0 and 39.9",
+        info: "Obese",
+        obesity: "II",
+    },
+    {
+        min: 40,
+        max: 99,
+        classification: "Greater than 40.0",
+        info: "Morbid obesity",
+        obesity: "III",
+    },
+];
+
